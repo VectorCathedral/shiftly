52813a03232693edfedc04699c820723b03436fa
Vector Cathedral <145724632+VectorCathedral@users.noreply.github.com>
2026-07-22 08:32:13 +0100

    Remove password input from sign-in form
    Removed password input section from the sign-in form.

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -36,10 +36,6 @@ function SignIn() {
             <Input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
 
-        <!--  <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          </div>-->
 
           <Button type="submit" className="w-full">Sign in</Button>
         </form>
