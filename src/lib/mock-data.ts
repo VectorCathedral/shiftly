867f5cbf5e52f4451c048d2e1fbb43307d8f0a39
Vector Cathedral <145724632+VectorCathedral@users.noreply.github.com>
2026-07-24 13:35:14 +0100

    Update upload URL in mock-data.ts

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -86,7 +86,7 @@ export async function uploadSchedule(file: File) {
       formData.append("email",user.email);
     }
 
-    const response =await fetch("http://localhost:8000/upload", {
+    const response =await fetch("http://16.28.28.246:8000/upload", {
         method: "POST",
         body: formData,
     });
