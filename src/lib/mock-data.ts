c0d7529ff8cec4e8375d818809215b6e419986d1
VectorCathedral <03.reunion.trot@icloud.com>
2026-07-25 22:29:19 +0200

    fixed ip issue

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -86,7 +86,7 @@ export async function uploadSchedule(file: File) {
       formData.append("email",user.email);
     }
 
-    const response =await fetch("http://16.28.2.19:8000/upload", {
+    const response =await fetch("http://16.28.2.192:8000/upload", {
         method: "POST",
         body: formData,
     });
